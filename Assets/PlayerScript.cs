d5a245c8511f99d36712bdd0e889e1f9f62d94ad
David Žižlavský <davidcitron2@seznam.cz>
2024-03-18 18:56:19 +0100

    Přidán upgrade pro pasivní příjem cookies

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -7,12 +7,15 @@ public class PlayerScript : MonoBehaviour
 {
     [SerializeField] private int cookies;
     [SerializeField] private TMP_Text cookieCounterText;
+    [SerializeField] private int passiveIncome;
 
     // Start is called before the first frame update
     void Start()
     {
         cookies = 0;
+        passiveIncome = 0;
         UpdateCookieCounter();
+        StartCoroutine(PassiveIncome());
     }
 
     // Update is called once per frame
@@ -30,5 +33,25 @@ public class PlayerScript : MonoBehaviour
     private void UpdateCookieCounter()
     {
         cookieCounterText.text = "Cookies: " + cookies;
+    }
+
+    public void BuyUpgrade()
+    {
+        if (cookies >= 10)
+        {
+            cookies -= 10;
+            UpdateCookieCounter();
+            passiveIncome++;
+        }
+    }
+
+    IEnumerator PassiveIncome()
+    {
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(1);
+            cookies += passiveIncome;
+            UpdateCookieCounter();
+        }
     }
 }
