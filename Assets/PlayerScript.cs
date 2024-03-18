7b0f36f51f12e8428fc31dce65cef95fc6c6f23c
David Žižlavský <davidcitron2@seznam.cz>
2024-03-18 18:17:52 +0100

    Přidání systému přepisování aktuální hodnoty cookies

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
     [SerializeField] private int cookies;
-    
+    [SerializeField] private TMP_Text cookieCounterText;
+
     // Start is called before the first frame update
     void Start()
     {
         cookies = 0;
+        UpdateCookieCounter();
     }
 
     // Update is called once per frame
@@ -21,5 +24,11 @@ public class PlayerScript : MonoBehaviour
     public void AddCookie()
     {
         cookies++;
+        UpdateCookieCounter();
+    }
+
+    private void UpdateCookieCounter()
+    {
+        cookieCounterText.text = "Cookies: " + cookies;
     }
 }
